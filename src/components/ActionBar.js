5e2dbf93111f8e162218256c2a71fdc0081bb6a3
premnarayanp <premnarayanpatel223@gmail.com>
2023-07-13 11:33:08 +0530

    Display styles modified

--- src/components/ActionBar.js
+++ src/components/ActionBar.js
@@ -6,37 +6,37 @@ import "../styles/actionBar.css";
     <div className='action-bar'>
       <table className="action-btn-table">
         <tr>
-          <td>C</td>
-          <td>+/-</td>
-          <td>%</td>
-          <td className='operators-btn'>%</td>
+          <td data-value="C">C</td>
+          <td data-value="+/-">+/-</td>
+          <td data-value="%">%</td>
+          <td data-value="/" className='operators-btn'>/</td>
         </tr>
 
         <tr>
-          <td>7</td>
-          <td>8</td>
-          <td>9</td>
-          <td className='operators-btn'>*</td>
+          <td data-value="7">7</td>
+          <td data-value="8">8</td>
+          <td data-value="9">9</td>
+          <td data-value="*" className='operators-btn'>*</td>
         </tr>
 
         <tr>
-          <td>4</td>
-          <td>5</td>
-          <td>6</td>
-          <td className='operators-btn'>-</td>
+          <td data-value="4">4</td>
+          <td data-value="5">5</td>
+          <td data-value="6">6</td>
+          <td data-value="-" className='operators-btn'>-</td>
         </tr>
 
         <tr>
-          <td>1</td>
-          <td>2</td>
-          <td>3</td>
-          <td className='operators-btn'>+</td>
+          <td data-value="1">1</td>
+          <td data-value="2">2</td>
+          <td data-value="3">3</td>
+          <td data-value="+" className='operators-btn'>+</td>
         </tr>
 
         <tr>
-          <td colspan='2'>0</td>
-          <td>.</td>
-          <td className='operators-btn'>=</td>
+          <td colspan='2' data-value="0">0</td>
+          <td data-value=".">.</td>
+          <td data-value="=" className='operators-btn'>=</td>
         </tr>
 
       </table>
--- src/components/Display.js
+++ src/components/Display.js
@@ -3,6 +3,7 @@ import "../styles/display.css";
  const Display=()=>{
   return (
     <div className='display'>
+      <p id='result'>012345678912345678</p>
     </div>
   );
    
